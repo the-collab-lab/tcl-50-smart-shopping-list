9583408222f2dfb2ae5d29c40f050c4e514cde1d
mide358 <iyimide.adegunloye@gmail.com>
2022-10-07 16:46:54 +0100

    placed map in return statement and used listitem component
    Signed-off-by: mide358 <iyimide.adegunloye@gmail.com>

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,15 +1,14 @@
 import { ListItem } from '../components';
 
 export function List({ data }) {
-	const list = data.map((listItem) => (
-		<li key={listItem.id}>{listItem.name}</li>
-	));
 	return (
 		<>
 			<p>
 				Hello from the <code>/list</code> page!
 			</p>
-			<ul>{list}</ul>
+			{data.map((data) => (
+				<ul>{ListItem(data)}</ul>
+			))}
 		</>
 	);
 }
