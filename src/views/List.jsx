fc6a0a145344b45cdae87488aa7dc0de454cc614
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-10-19 16:16:57 +0400

    set up form

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const [searchItem, setSearchItem] = useState('');
+
 	return (
 		<>
-			<p>
-				Hello from the <code>/list</code> page!
-			</p>
+			<form>
+				<label htmlFor="filter_items">Filter items </label>
+				<input
+					type="text"
+					value={searchItem}
+					id="filter_items"
+					placeholder="Start typing here..."
+					onChange={(e) => setSearchItem(e.target.value)}
+				/>
+			</form>
 			<ul>
 				{data.map((item) => (
 					<ListItem name={item.name} key={item.id} />
