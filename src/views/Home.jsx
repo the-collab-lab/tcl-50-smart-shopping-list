239b4a10015cc335b62d908d95838c6414f5a860
Victor Owiti <viowiti12@gmail.com>
2022-10-19 16:38:35 +0300

    Added a form that allows the user to input an existing token

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,34 @@
+import { useState } from 'react';
 import './Home.css';
 
 export function Home(props) {
+	const [token, setToken] = useState('');
 	const { onClick } = props;
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log(token);
+	}
 
 	return (
 		<div className="Home">
 			<button onClick={onClick} className="create-list-btn">
 				Create a new list
 			</button>
+
+			<form onSubmit={handleSubmit}>
+				<div>
+					<label htmlFor="join-list">Join an existing shopping list</label>
+					<input
+						value={token}
+						onChange={(e) => setToken(e.target.value)}
+						type="text"
+						name="join-list"
+						id="join-list"
+					/>
+				</div>
+				<button type="submit">Join Shopping List</button>
+			</form>
 		</div>
 	);
 }
