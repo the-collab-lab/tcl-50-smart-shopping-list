8d1963611b28a2ac1e594f7698c6a8aa7dc87504
Klesta Luli <74952593+klezi10@users.noreply.github.com>
2022-10-25 14:31:05 +0200

    added welcoming prompt when there is nothing in the list
    Co-authored-by: Victor <viowiti12@gmail.com>
    Co-authored-by: Klesta <klesta.luli@gmail.com>

--- src/views/List.jsx
+++ src/views/List.jsx
@@ -1,7 +1,9 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { ListItem } from '../components';
 
 export function List({ data }) {
+	const navigate = useNavigate();
 	const [searchItem, setSearchItem] = useState('');
 
 	const filteredItems = data?.filter((item) =>
@@ -10,21 +12,30 @@ export function List({ data }) {
 
 	return (
 		<>
-			<form>
-				<label htmlFor="filter_items">Filter items </label>
-				<input
-					type="search"
-					value={searchItem}
-					id="filter_items"
-					placeholder="Start typing here..."
-					onChange={(e) => setSearchItem(e.target.value)}
-				/>
-			</form>
-			<ul>
-				{filteredItems.map((item) => (
-					<ListItem name={item.name} key={item.id} />
-				))}
-			</ul>
+			{data.length ? (
+				<>
+					<form>
+						<label htmlFor="filter_items">Filter items </label>
+						<input
+							type="search"
+							value={searchItem}
+							id="filter_items"
+							placeholder="Start typing here..."
+							onChange={(e) => setSearchItem(e.target.value)}
+						/>
+					</form>
+					<ul>
+						{filteredItems.map((item) => (
+							<ListItem name={item.name} key={item.id} />
+						))}
+					</ul>
+				</>
+			) : (
+				<>
+					<p>Your shopping list is currently empty.</p>
+					<button onClick={() => navigate('/add-item')}>Add Item</button>
+				</>
+			)}
 		</>
 	);
 }
