77ac446459b2309721ee450613cac1d4e0f7ca37
Victor Owiti <viowiti12@gmail.com>
2022-11-03 10:45:53 +0300

    Added error message in case item isnt added to list

--- src/views/AddItem.jsx
+++ src/views/AddItem.jsx
@@ -5,37 +5,48 @@ export function AddItem({ data, listToken }) {
 	const [newItem, setNewItem] = useState('');
 	const [nextPurchaseTime, setPurchaseTime] = useState(7);
 	const [statusMessage, setStatusMessage] = useState('');
-	const [styles, setStyles] = useState(false);
 
-	//map old items and remove punctuation with regex
+	//map through existing list items and remove punctuation and spaces  with regex
 	const removePunc = data?.map((item) =>
 		item?.name.toLowerCase().replace(/[^a-zA-Z0-9]/g, ''),
 	);
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
+
 		if (newItem.trim().length === 0) {
 			setStatusMessage('Please enter an item name');
-			setStyles(true);
-			return;
+			setTimeout(() => {
+				setStatusMessage('');
+				setNewItem('');
+			}, 3000);
 		} else if (
 			removePunc?.includes(newItem?.toLowerCase().replace(/[^a-z0-9]/gi, '')) //check for duplication
 		) {
-			setStatusMessage(`${newItem} already exist`);
-			setStyles(true);
-			return;
+			setStatusMessage(`${newItem} already exists`);
+			setTimeout(() => {
+				setStatusMessage('');
+				setNewItem('');
+			}, 3000);
 		} else {
 			const item = await addItem(listToken, {
 				itemName: newItem,
 				daysUntilNextPurchase: nextPurchaseTime,
 			});
-			setStatusMessage(` ${newItem} successfully added`);
-			setNewItem('');
-			setPurchaseTime(7);
-			setStyles(false);
-			setTimeout(() => {
-				setStatusMessage('');
-			}, 3000);
+
+			if (item.id) {
+				setStatusMessage(` ${newItem} successfully added`);
+				setNewItem('');
+				setPurchaseTime(7);
+				setTimeout(() => {
+					setStatusMessage('');
+				}, 3000);
+			} else {
+				setStatusMessage(` ${newItem} was not added`);
+				setTimeout(() => {
+					setStatusMessage('');
+				}, 3000);
+			}
 		}
 	};
 
@@ -96,9 +107,7 @@ export function AddItem({ data, listToken }) {
 					</fieldset>
 				</div>
 				<button type="submit">Add Item</button>
-				{statusMessage && (
-					<p style={{ color: styles ? 'red' : 'black' }}>{statusMessage}</p>
-				)}
+				{statusMessage && <p>{statusMessage}</p>}
 			</form>
 		</>
 	);
