6c65de46dd4ff6d8300b90ec90933a605be4a704
mide358 <iyimide.adegunloye@gmail.com>
2022-11-26 20:14:10 +0100

    Cleaned up the code

--- src/App.jsx
+++ src/App.jsx
@@ -15,16 +15,6 @@ export function App() {
 	const [data, setData] = useState([]);
 	const [isLoading, setIsLoading] = useState(true);
 
-	/**
-	 * Here, we're using a custom hook to create `listToken` and a function
-	 * that can be used to update `listToken` later.
-	 *
-	 * `listToken` is `my test list` by default so you can see the list
-	 * of items that was prepopulated for this project.
-	 * You'll later set it to `null` by default (since new users do not
-	 * have tokens), and use `setListToken` when you allow a user
-	 * to create and join a new list.
-	 */
 	const [formToken, setFormToken] = useState('');
 
 	const [listToken, setListToken] = useStateWithStorage(
@@ -53,25 +43,9 @@ export function App() {
 	useEffect(() => {
 		if (!listToken) return;
 
-		/**
-		 * streamListItems` takes a `listToken` so it can commuinicate
-		 * with our database; then calls a callback function with
-		 * a `snapshot` from the database.
-		 *
-		 * Refer to `api/firebase.js`.
-		 */
 		return streamListItems(listToken, (snapshot) => {
-			/**
-			 * Read the documents in the snapshot and do some work
-			 * on them, so we can save them in our React state.
-			 *
-			 * Refer to `api/firebase.js`
-			 *
-			 */
-
 			const nextData = getItemData(snapshot);
 
-			/** Finally, we update our React state. */
 			setData(nextData);
 			setIsLoading(false);
 		});
